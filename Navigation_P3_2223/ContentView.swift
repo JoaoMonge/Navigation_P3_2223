8cbf41fde30b84a2d7bc7585a8ed3ea242a3bd47
João Monge <joao.monge13@gmail.com>
2022-11-17 09:56:32 +0000

    Navegação TabView

--- Navigation_P3_2223/ContentView.swift
+++ Navigation_P3_2223/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
+        /*NavigationStack{
             Page1()
+        }*/
+        
+        TabView{
+            Page1().tabItem {
+                Label("Page 1",systemImage: "square.and.arrow.up.on.square")
+            }
+            Page2().tabItem {
+                Label("Page 2",systemImage: "pencil.circle.fill")
+            }
+            Page3().tabItem {
+                Label("Page 3",systemImage: "plus.rectangle.on.folder.fill")
+            }
         }
     }
 }
