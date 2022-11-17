277a14bb07ee75428e59e12bee5944a2016b7d02
João Monge <joao.monge13@gmail.com>
2022-11-17 09:08:36 +0000

    Initial Commit

---
+++ Navigation_P3_2223/Navigation_P3_2223App.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_P3_2223App.swift
+//  Navigation_P3_2223
+//
+//  Created by João Monge on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_P3_2223App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
